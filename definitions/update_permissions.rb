d65bd00e9d81781bb7f033fd9fb08ced7dbdca6e
Ptah Dunbar <yarr@piratedunbar.com>
2013-12-16 15:51:40 -0500

    use sudo to prevent errors.

--- definitions/update_permissions.rb
+++ definitions/update_permissions.rb
@@ -1,8 +1,8 @@
 define :update_permissions, :dmode => '775', :fmode => '664', :group => nil, :user => '' do
-	execute "update permissions in #{params[:name]}" do
-    	command "find #{params[:name]} -type d -exec chmod #{params[:dmode]} {} \;"
-    	command "find #{params[:name]} -type f -exec chmod #{params[:fmode]} {} \;"
-    	command "chown -R #{params[:user]}:#{params[:group]} #{params[:name]}"
-    	not_if { ! params[:name] }
+    execute "update permissions in #{params[:name]}" do
+        command "sudo find #{params[:name]} -type d -exec chmod #{params[:dmode]} {} \;"
+        command "sudo find #{params[:name]} -type f -exec chmod #{params[:fmode]} {} \;"
+        command "sudo chown -R #{params[:user]}:#{params[:group]} #{params[:name]}"
+        not_if { ! params[:name] }
     end
 end
